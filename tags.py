0e8dab2004237c2a4d062f7b545f6764cc330c67
Bastian Greshake <bgreshake@googlemail.com>
2012-08-04 10:11:35 +0200

    save upload date & length as well

--- tags.py
+++ tags.py
@@ -7,23 +7,31 @@ import sys
 from bs4 import BeautifulSoup
 
 def GetVideosForPage(soup):
+    '''
+    Grab all Video-Links from single tag-page, like
+    http://video.xnxx.com/tags/acrobat
+    '''
     video_urls = []
-    for a in soup.findAll("a"):
+    for a in soup.findAll("a"): # iterate over all <a></a>-tags in soup. Return link to video if found
         if a.has_key("class"):
             if a["class"] == "miniature":
                 video_urls.append(a["href"])
     return video_urls
     
 def IterateTagPages(url):
-    url = urllib2.urlopen(url)
+    '''
+    Iterate over all video-pages for a single tag. E.g. get "acrobat"-videos
+    from page 1 to 8:  
+    '''
+    url = urllib2.urlopen(url) # again, reading the site
     urlcontent = url.read()
     soup = BeautifulSoup(urlcontent,"xml")
     video_urls = []
-    video_urls.extend(GetVideosForPage(soup))
+    video_urls.extend(GetVideosForPage(soup)) # give soup to function to find all videos on initial page
     next_url = ""
     next = False
     print len(video_urls)
-    for i in soup.findAll("a","nP"):
+    for i in soup.findAll("a","nP"): # check whether there are more pages to grab. if yes return url of next page and set flag to True
         if i.string == "Next":
             next = True
             next_url = "http://video.xnxx.com" + i["href"]
@@ -31,7 +39,7 @@ def IterateTagPages(url):
         else:
             next = False
                        
-    while next == True:
+    while next == True: # if there are more pages in forward direction: grab those as well. 
         print next_url
         url = urllib2.urlopen(next_url)
         urlcontent = url.read()
@@ -48,6 +56,10 @@ def IterateTagPages(url):
     return video_urls
 
 def VideosForTag(video_collection,url):
+    '''
+    Iterate over all videos in array and return a dictionary with
+    key = videourl, value = video-object, see videos.py
+    '''
     print "getting video-urls for tag"
     video_urls = IterateTagPages(url)
     print "got video-urls"
@@ -59,6 +71,10 @@ def VideosForTag(video_collection,url):
     return video_collection
 
 def TagList(url):
+    '''
+    Set up initial tag-list from URL. 
+    Take http://video.xnxx.com/tags/ and iterate over all tags and get list of links to each tag
+    '''
     url = urllib2.urlopen(url)
     urlcontent = url.read()
     soup = BeautifulSoup(urlcontent,"xml")
@@ -71,16 +87,19 @@ def TagList(url):
     return tags
 
 def IterateTags(url):
+    '''
+    Load or get all tags, iterate over each tag to get videos & tags
+    '''
     try:
-        video_pickle = open("video_collection.pickle","rb")
+        video_pickle = open("video_collection.pickle","rb") # if we already ran the script there should be videos to save
         video_collection = pickle.load(video_pickle)
         video_pickle.close()
         print "loaded saved videos"
     except:
-        print "created new video-collection"
+        print "created new video-collection"        # if not create new collection
         video_collection = {}
     try:
-        tag_pickle = open("tag_collection.pickle","rb")
+        tag_pickle = open("tag_collection.pickle","rb") # same is true for tags
         tag_collection = pickle.load(tag_pickle)
         tag_pickle.close()
         print "loaded tag-collection"
@@ -89,8 +108,8 @@ def IterateTags(url):
         tag_collection = TagList(url)
         print "got new tags"
     
-    for tag,value in tag_collection.iteritems():
-        if value == False:
+    for tag,value in tag_collection.iteritems():    # now iterate over each tag
+        if value == False:  # if it already was parsed (value == True: great, we skip it)
             print "############################################"
             print "iterate over videos for %s" % (tag)
             video_collection = VideosForTag(video_collection,"http://video.xnxx.com"+tag)
